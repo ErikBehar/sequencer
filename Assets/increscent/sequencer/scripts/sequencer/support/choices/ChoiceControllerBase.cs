47a6b5afd5bab88b7ada17ba545540c16d151cc2
Erik <invadererik@gmail.com>
2014-12-14 18:39:04 -0800

    bleh hopefully fixed overwriting some stuff

--- Assets/increscent/sequencer/scripts/sequencer/support/choices/ChoiceControllerBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class ChoiceControllerBase : MonoBehaviour
-{
-    public abstract void generateButtons(List<ChoiceModel> choices, SequencePlayer player);
-    public abstract void cleanup();
-}
--- Assets/increscent/sequencer/support/choices/ChoiceModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ChoiceModel
-{
-    public string text;
-    public string sceneNameToJump;
-    public int sceneCommandIndexToJump = 0;
-}
